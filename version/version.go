a8ccd41c9b6d23f79f740453f663e94925a66e0b
Rohil Surana <rohilsurana96@gmail.com>
2021-10-14 14:33:58 +0530

    feat: add version cli command

--- version/version.go
+++ version/version.go
@@ -1,10 +1,12 @@
 package version
 
 import (
+	"fmt"
 	"runtime"
 	"time"
 
 	commonv1 "go.buf.build/odpf/gw/odpf/proton/odpf/common/v1"
+	"google.golang.org/protobuf/encoding/protojson"
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
@@ -23,9 +25,14 @@ func GetVersionAndBuildInfo() *commonv1.Version {
 		Architecture: runtime.GOARCH,
 	}
 
-	if bt, err := time.Parse(time.UnixDate, BuildTime); err == nil {
+	if bt, err := time.Parse(time.RFC3339, BuildTime); err == nil {
 		v.BuildTime = timestamppb.New(bt)
 	}
 
 	return v
+}
+
+func Print() error {
+	_, err := fmt.Println(protojson.Format(GetVersionAndBuildInfo()))
+	return err
 }
