e0bd7215db1ba8a0e576bc7f42fbb8159ae2e926
Ishan Arya <ishanarya0@gmail.com>
2022-03-03 18:18:35 +0530

    chore: update mock file

--- mocks/resource_service.go
+++ mocks/resource_service.go
@@ -69,6 +69,53 @@ func (_c *ResourceService_CreateResource_Call) Return(_a0 *domain.Resource, _a1 
 	return _c
 }
 
+// DeleteResource provides a mock function with given fields: ctx, res
+func (_m *ResourceService) DeleteResource(ctx context.Context, res *domain.Resource) (*domain.Resource, error) {
+	ret := _m.Called(ctx, res)
+
+	var r0 *domain.Resource
+	if rf, ok := ret.Get(0).(func(context.Context, *domain.Resource) *domain.Resource); ok {
+		r0 = rf(ctx, res)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(*domain.Resource)
+		}
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(context.Context, *domain.Resource) error); ok {
+		r1 = rf(ctx, res)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
+// ResourceService_DeleteResource_Call is a *mock.Call that shadows Run/Return methods with type explicit version for method 'DeleteResource'
+type ResourceService_DeleteResource_Call struct {
+	*mock.Call
+}
+
+// DeleteResource is a helper method to define mock.On call
+//  - ctx context.Context
+//  - res *domain.Resource
+func (_e *ResourceService_Expecter) DeleteResource(ctx interface{}, res interface{}) *ResourceService_DeleteResource_Call {
+	return &ResourceService_DeleteResource_Call{Call: _e.mock.On("DeleteResource", ctx, res)}
+}
+
+func (_c *ResourceService_DeleteResource_Call) Run(run func(ctx context.Context, res *domain.Resource)) *ResourceService_DeleteResource_Call {
+	_c.Call.Run(func(args mock.Arguments) {
+		run(args[0].(context.Context), args[1].(*domain.Resource))
+	})
+	return _c
+}
+
+func (_c *ResourceService_DeleteResource_Call) Return(_a0 *domain.Resource, _a1 error) *ResourceService_DeleteResource_Call {
+	_c.Call.Return(_a0, _a1)
+	return _c
+}
+
 // GetResource provides a mock function with given fields: ctx, urn
 func (_m *ResourceService) GetResource(ctx context.Context, urn string) (*domain.Resource, error) {
 	ret := _m.Called(ctx, urn)
