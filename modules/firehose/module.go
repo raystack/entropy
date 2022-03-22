8e80de92bbebc89b08c47a3eca101b975ae4f642
Ishan Arya <ishanarya0@gmail.com>
2022-03-22 13:30:05 +0530

    feat: update config schema for firehose module

--- modules/firehose/module.go
+++ modules/firehose/module.go
@@ -15,32 +15,72 @@ const configSchemaString = `
 	"$schema": "http://json-schema.org/draft-04/schema#",
 	"type": "object",
 	"properties": {
-		"name": { "type": "string" },
-		"title": { "type": "string" },
-		"stream_name": { "type": "string" },
-		"topic_name": { "type": "string" },
 		"image": { "type": "string" },
-		"configuration": {},
+		"configuration": {
+			"type": "object",
+			"properties": {
+				"SOURCE_KAFKA_BROKERS": { "type": "string" },
+				"SOURCE_KAFKA_TOPIC": { "type": "string" },
+				"SOURCE_KAFKA_CONSUMER_GROUP_ID": { "type": "string" },
+				"INPUT_SCHEMA_PROTO_CLASS": { "type": "string" },
+			}
+		},
 		"replicas": { "type": "number" },
 		"namespace": { "type": "string" },
 		"cluster": { "type": "string" },
-		"organization": { "type": "string" },
-		"entity": { "type": "string" },
-		"consumer_group_id": { "type": "string" },
-		"team": { "type": "string" },
-		"landscape": { "type": "string" },
-		"environment": { "type": "string", "enum": [ "integration", "production" ] },
-		"projectID": { "type": "string" },
-		"sink_type": { "type": "string" },
-		"state": { "type": "string", "enum": [ "running", "stopped" ] },
+		"sink_type": { "type": "string",},
+		"state": { "type": "string", "enum": [ "RUNNING", "STOPPED" ] },
 		"stop_date": { "type": "string", "format": "date-time" },
 		"description": { "type": "string" },
-		"created_by": { "type": "string" },
-		"updated": { "type": "string", "format": "date-time" },
-		"created": { "type": "string", "format": "date-time" }
-	  },
-	  "required": [ "title", "stream_name", "image", "replicas", "namespace", "state" ]
-  }
+	},
+	"oneOf": [
+		{
+		  "if": {
+			"properties": { "sink_type": { "const": "LOG" } }
+		  },
+		  "then": {
+			"properties": { "configuration": { 
+				"KAFKA_RECORD_PARSER_MODE": { "type": "string" },
+				"SOURCE_KAFKA_BROKERS": { "type": "string" },
+				"SOURCE_KAFKA_TOPIC": { "type": "string" },
+				"SOURCE_KAFKA_CONSUMER_GROUP_ID": { "type": "string" },
+				"INPUT_SCHEMA_PROTO_CLASS": { "type": "string" },
+			 } }
+		  }
+		},
+		{
+		  "if": {
+			"properties": { "country": { "const": "HTTP" } },
+		  },
+		  "then": {
+			"properties": { "configuration": {
+				"SOURCE_KAFKA_BROKERS": { "type": "string" },
+				"SOURCE_KAFKA_TOPIC": { "type": "string" },
+				"SOURCE_KAFKA_CONSUMER_GROUP_ID": { "type": "string" },
+				"INPUT_SCHEMA_PROTO_CLASS": { "type": "string" },
+				"SINK_HTTP_RETRY_STATUS_CODE_RANGES": { "type": "string" },
+				"SINK_HTTP_REQUEST_LOG_STATUS_CODE_RANGES": { "type": "string" },
+				"SINK_HTTP_REQUEST_TIMEOUT_MS": { "type": "number" },
+				"SINK_HTTP_REQUEST_METHOD": { "type": "string", "enum": [ "put", "post" ] },
+				"SINK_HTTP_MAX_CONNECTIONS": { "type": "number" },
+				"SINK_HTTP_SERVICE_URL": { "type": "string" },
+				"SINK_HTTP_HEADERS": { "type": "string" },
+				"SINK_HTTP_PARAMETER_SOURCE": { "type": "string", "enum": [ "key", "message", "disabled" ] },
+				"SINK_HTTP_DATA_FORMAT": { "type": "string", "enum": [ "proto", "json" ] },
+				"SINK_HTTP_OAUTH2_ENABLE": { "type": "boolean" },
+				"SINK_HTTP_OAUTH2_ACCESS_TOKEN_URL": { "type": "string" },
+				"SINK_HTTP_OAUTH2_CLIENT_NAME": { "type": "string" },
+				"SINK_HTTP_OAUTH2_CLIENT_SECRET": { "type": "string" },
+				"SINK_HTTP_OAUTH2_SCOPE": { "type": "string" },
+				"SINK_HTTP_JSON_BODY_TEMPLATE": { "type": "string" },
+				"SINK_HTTP_PARAMETER_PLACEMENT": { "type": "string", "enum": [ "query", "header" ] },
+				"SINK_HTTP_PARAMETER_SCHEMA_PROTO_CLASS": { "type": "string" },
+			 } }
+		  }
+		},
+	],
+	"required": [ "image", "replicas", "namespace", "state" ]
+}
 `
 
 type Module struct {
