f0b99b23b52cda8d9960ebe1f6c6781be42c9bde
Ishan Arya <ishanarya0@gmail.com>
2022-05-04 15:05:45 +0530

    refactor: refactor streamFromAPod & streamFromAllPods into one function

--- plugins/providers/kubelogger/logger.go
+++ plugins/providers/kubelogger/logger.go
@@ -20,7 +20,8 @@ import (
 
 func GetStreamingLogs(ctx context.Context, namespace string, filter map[string]string, cfg rest.Config) (<-chan resource.LogChunk, error) {
 	var selectors []string
-	var podName, containerName string
+	var podName, containerName, labelSelector, filedSelector string
+	var opts metav1.ListOptions
 
 	clientSet, err := kubernetes.NewForConfig(&cfg)
 	if err != nil {
@@ -40,15 +41,18 @@ func GetStreamingLogs(ctx context.Context, namespace string, filter map[string]s
 	}
 
 	if podName == "" {
-		selector := strings.Join(selectors, ",")
-		return streamFromAllPods(ctx, clientSet, namespace, selector, filter)
+		labelSelector = strings.Join(selectors, ",")
+		opts = metav1.ListOptions{LabelSelector: labelSelector}
 	} else {
-		return streamFromOnePod(ctx, clientSet, namespace, podName, containerName, filter)
+		filedSelector = fmt.Sprintf("metadata.name=%s", podName)
+		opts = metav1.ListOptions{FieldSelector: filedSelector}
 	}
+
+	return streamFromPods(ctx, clientSet, namespace, containerName, opts, filter)
 }
 
-func streamFromAllPods(ctx context.Context, clientSet *kubernetes.Clientset, namespace, selector string, filter map[string]string) (<-chan resource.LogChunk, error) {
-	pods, err := clientSet.CoreV1().Pods(namespace).List(ctx, metav1.ListOptions{LabelSelector: selector})
+func streamFromPods(ctx context.Context, clientSet *kubernetes.Clientset, namespace, containerName string, opts metav1.ListOptions, filter map[string]string) (<-chan resource.LogChunk, error) {
+	pods, err := clientSet.CoreV1().Pods(namespace).List(ctx, opts)
 	if err != nil {
 		return nil, err
 	}
@@ -58,6 +62,9 @@ func streamFromAllPods(ctx context.Context, clientSet *kubernetes.Clientset, nam
 	wg := &sync.WaitGroup{}
 	for _, pod := range pods.Items {
 		for _, container := range append(pod.Spec.InitContainers, pod.Spec.Containers...) {
+			if containerName != "" && container.Name != containerName {
+				continue
+			}
 			wg.Add(1)
 			go func(podName string, c v1.Container) {
 				defer wg.Done()
@@ -66,37 +73,6 @@ func streamFromAllPods(ctx context.Context, clientSet *kubernetes.Clientset, nam
 				}
 			}(pod.Name, container)
 		}
-	}
-
-	go func() {
-		wg.Wait()
-		close(logCh)
-	}()
-
-	return logCh, nil
-}
-
-func streamFromOnePod(ctx context.Context, clientSet *kubernetes.Clientset, namespace, podName, containerName string, filter map[string]string) (<-chan resource.LogChunk, error) {
-	pod, err := clientSet.CoreV1().Pods(namespace).Get(ctx, podName, metav1.GetOptions{})
-	if err != nil {
-		return nil, err
-	}
-
-	logCh := make(chan resource.LogChunk)
-
-	wg := &sync.WaitGroup{}
-
-	for _, container := range append(pod.Spec.InitContainers, pod.Spec.Containers...) {
-		if containerName != "" && container.Name != containerName {
-			continue
-		}
-		wg.Add(1)
-		go func(podName string, c v1.Container) {
-			defer wg.Done()
-			if err := streamContainerLogs(ctx, namespace, podName, logCh, clientSet, c, filter); err != nil {
-				log.Printf("[WARN] failed to stream from container '%s':%s", c.Name, err)
-			}
-		}(pod.Name, container)
 	}
 
 	go func() {
